a790736dd97daf4d490d6215df7008b3d44c6e7f
alexei-yasko <alexei.yasko@gmail.com>
2013-09-19 12:58:36 +0300

    interface updated

--- yaskoam.mrz2.lab1/src/main/scala/yaskoam/mrz2/lab1/ui/controllers/MainSceneController.scala
+++ yaskoam.mrz2.lab1/src/main/scala/yaskoam/mrz2/lab1/ui/controllers/MainSceneController.scala
@@ -2,13 +2,14 @@ package yaskoam.mrz2.lab1.ui.controllers
 
 import javafx.fxml.FXML
 import javafx.scene.image.{Image, ImageView}
-import javafx.event.{Event, ActionEvent}
+import javafx.event.{EventHandler, Event, ActionEvent}
 import javafx.stage.FileChooser
 import javafx.stage.FileChooser.ExtensionFilter
-import java.io.FileInputStream
+import java.io.{File, FileInputStream}
 import javafx.scene.layout.VBox
-import javafx.scene.control.{Tab, MenuItem}
+import javafx.scene.control.{Label, Tab, MenuItem}
 import javafx.application.Platform
+import javafx.scene.input.MouseEvent
 
 /**
  * @author Q-YAA
@@ -24,23 +25,21 @@ class MainSceneController {
   @FXML
   var sourceImage: ImageView = null
 
+
+  @FXML
+  var imageHeightLabel: Label = null
+
+  @FXML
+  var imageWidthLabel: Label = null
+
   def loadSourceImage(event: ActionEvent) {
-
-    val fileChooser = new FileChooser()
-    fileChooser.setTitle("Choose Image")
-    fileChooser.getExtensionFilters.addAll(
-      new ExtensionFilter("JPEG", "*.jpg"),
-      new ExtensionFilter("BMP", "*.bmp"),
-      new ExtensionFilter("PNG", "*.png")
-    )
-
-    val file = fileChooser.showOpenDialog(rootPane.getScene.getWindow)
+    val file = createFileChooser().showOpenDialog(rootPane.getScene.getWindow)
 
     if (file != null) {
-      val imageStream = new FileInputStream(file)
-      val image = new Image(imageStream)
+      val image = getImage(file)
       sourceImage.setImage(image)
-      imageStream.close()
+
+      setSourceImageEventHandlers()
     }
   }
 
@@ -50,5 +49,60 @@ class MainSceneController {
 
   def disableLoadImageMenuItem(event: Event) {
     loadSourceImageMenuItem.setDisable(!event.getSource.asInstanceOf[Tab].isSelected)
+  }
+
+  def chooseImage(event: MouseEvent) {
+    val imageView: ImageView = event.getSource.asInstanceOf[ImageView]
+    imageView.scaleXProperty().setValue(1.1)
+    imageView.scaleYProperty().setValue(1.1)
+
+    imageHeightLabel.setText("image height: " + imageView.getImage.getHeight.toString)
+    imageWidthLabel.setText("image width:" + imageView.getImage.getWidth.toString)
+  }
+
+  def unChooseImage(event: MouseEvent) {
+    val imageView: ImageView = event.getSource.asInstanceOf[ImageView]
+    imageView.scaleXProperty().setValue(1)
+    imageView.scaleYProperty().setValue(1)
+
+    imageHeightLabel.setText("")
+    imageWidthLabel.setText("")
+  }
+
+  private def getImage(file: File): Image = {
+    val imageStream = new FileInputStream(file)
+    val image = new Image(imageStream)
+    imageStream.close()
+    image
+  }
+
+  private def createFileChooser(): FileChooser = {
+
+    val fileChooser = new FileChooser()
+
+    fileChooser.setTitle("Choose Image")
+
+    fileChooser.getExtensionFilters.addAll(
+      new ExtensionFilter("JPEG", "*.jpg"),
+      new ExtensionFilter("BMP", "*.bmp"),
+      new ExtensionFilter("PNG", "*.png")
+    )
+
+    fileChooser
+  }
+
+  private def setSourceImageEventHandlers() {
+
+    sourceImage.setOnMouseEntered(new EventHandler[MouseEvent]() {
+      def handle(event: MouseEvent) {
+        chooseImage(event)
+      }
+    })
+
+    sourceImage.setOnMouseExited(new EventHandler[MouseEvent]() {
+      def handle(event: MouseEvent) {
+        unChooseImage(event)
+      }
+    })
   }
 }

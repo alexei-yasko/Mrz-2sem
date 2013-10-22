ef623199dde7415c64f54d4d38710e2524d4cb35
alexei-yasko <fktrctq1992>
2013-10-22 22:09:21 +0300

    tool component bar added

---
+++ yaskoam.mrz2.lab2/src/main/java/yaskoam/mrz2/lab2/ui/toolbar/ToolBarPanel.java
@@ -0,0 +1,20 @@
+package yaskoam.mrz2.lab2.ui.toolbar;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import yaskoam.mrz2.lab2.ui.BaseComponent;
+
+/**
+ * @author Q-YAA
+ */
+public class ToolBarPanel extends BaseComponent {
+
+    @FXML
+    private Button stopButton;
+
+    @FXML
+    private Button compressButton;
+
+    public ToolBarPanel() {
+    }
+}
